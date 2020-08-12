df73bc4ef376c052fdbbe2c5c2961d417fad1388
Eitan Shaked <eitansh@mellanox.com>
2020-08-12 21:37:22 +0300

    Remove if function_exists

--- admin-order-message-customer.php
+++ admin-order-message-customer.php
@@ -9,37 +9,34 @@
 **/
 
 
-if ( !function_exists( 'admcw_woocommerce_addon_activate' ) ) {
-	// Check if woocommerce is active
-	function admcw_woocommerce_addon_activate() {
-		if( !class_exists( 'WooCommerce' ) ) {
-			deactivate_plugins( plugin_basename( __FILE__ ) );
-			wp_die( __( 'Please install and Activate WooCommerce.', 'admin-order-message-customer' ), 'Plugin dependency check', array( 'back_link' => true ) );
-		}
+// Check if woocommerce is active
+function admcw_woocommerce_addon_activate() {
+	if( !class_exists( 'WooCommerce' ) ) {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		wp_die( __( 'Please install and Activate WooCommerce.', 'admin-order-message-customer' ), 'Plugin dependency check', array( 'back_link' => true ) );
 	}
 }
+
 
 //sets up activation hook
 register_activation_hook(__FILE__, 'admcw_woocommerce_addon_activate');
 
 add_action( 'woocommerce_admin_order_data_after_billing_address', 'admcw_after_billing_address', 10, 3 );
-if ( !function_exists( 'admcw_after_billing_address' ) ) {
-	function admcw_after_billing_address( $order ){
-		$phone = $order->billing_phone;
-		if (!$phone)
-			# Return if there is no billing phone
-			return;
-		# If Billing country is Israel then need to take care of country code
-		if ($order->billing_country == "IL"){
-			$pattern = '/^05/i';
-			$replacement = '+9725';
-			$phone =  preg_replace($pattern, $replacement, $phone);
-		}
-		
-		echo '<p>
-		<strong>WHATSAPP:</strong>
-		<a target="_blank" href="'.esc_url("https://api.whatsapp.com/send?phone=".$phone).'">'.$phone.'</a>
-		</p>
-		';
+function admcw_after_billing_address( $order ){
+	$phone = $order->billing_phone;
+	if (!$phone)
+		# Return if there is no billing phone
+		return;
+	# If Billing country is Israel then need to take care of country code
+	if ($order->billing_country == "IL"){
+		$pattern = '/^05/i';
+		$replacement = '+9725';
+		$phone =  preg_replace($pattern, $replacement, $phone);
 	}
+	
+	echo '<p>
+	<strong>WHATSAPP:</strong>
+	<a target="_blank" href="'.esc_url("https://api.whatsapp.com/send?phone=".$phone).'">'.$phone.'</a>
+	</p>
+	';
 }
